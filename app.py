a7849f8418337f7fbc38f1c3b5d6713c7b9f5333
jnb27 <jnb27@njit.edu>
2021-03-22 15:50:40 -0400

    Trying to clean app.py

--- app.py
+++ app.py
@@ -20,31 +20,19 @@ channel.queue_declare(queue='hello')
 
 @app.route('/form',methods = ['GET', 'POST'])
 def form():
-<<<<<<< HEAD
-    userR = str(request.form['username'])
-    passR = str(request.form['password'])
-    print(userR, passR)
-=======
     newPass = str(request.form['password'])
     newInfo = str(request.form['username'])
     print(newPass,newInfo)
->>>>>>> fceab8b1c16da763539ea66f989c81c17c7e6b36
     return render_template('login.html')
     
     
 @app.route('/start',methods = ['GET', 'POST'])
 def start():
-<<<<<<< HEAD
-
     #Handle if queue gives us a pass or fail from the login
 
     info = str(request.form['username'])
     passw = str(request.form['password'])
     channel.basic_publish(exchange='', routing_key='hello', body= info)
-=======
-    passw = str(request.form['password'])
-    info = str(request.form['username'])
->>>>>>> fceab8b1c16da763539ea66f989c81c17c7e6b36
     print(info,passw)
     return render_template('index.html')
     
@@ -56,8 +44,4 @@ app.run(
    host=os.getenv('IP','0.0.0.0'),
    debug=True
    )  
-<<<<<<< HEAD
 
-=======
-    
->>>>>>> fceab8b1c16da763539ea66f989c81c17c7e6b36

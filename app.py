205e6fa132b4c4b50cd64b9a3ae005f990c9d2a5
jnb27 <jnb27@njit.edu>
2021-03-22 15:34:39 -0400

    This app can send only the user to RabMQ

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import requests
 import logging
 import os
 import random
+import pika
 from flask_rabmq import RabbitMQ
 #from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template, request
@@ -10,13 +11,33 @@ app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-@app.route('/',methods = ['GET', 'POST'])
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host ='25.121.196.54', port=5672, credentials=credentials))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+@app.route('/form',methods = ['GET', 'POST'])
 def form():
+    #userR = str(request.form['usernameR'])
+    #passR = str(request.form['passwordR'])
+    #print(userR, passR)
     return render_template('login.html')
+    
+    
 @app.route('/start',methods = ['GET', 'POST'])
 def start():
+
+    #Handle if queue gives us a pass or fail from the login
+
+    info = str(request.form['username'])
+    passw = str(request.form['password'])
+    channel.basic_publish(exchange='', routing_key='hello', body= info)
+    print(info,passw)
     return render_template('index.html')
-@app.route('/newUser',methods = ['GET', 'POST'])
+    
+@app.route('/',methods = ['GET', 'POST'])
 def newUser():
     return render_template('register.html')
 app.run(
@@ -24,5 +45,4 @@ app.run(
    host=os.getenv('IP','0.0.0.0'),
    debug=True
    )  
-    
-    
+

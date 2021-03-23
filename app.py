459f55ddf57806e36eb2cb66135393ef413ee7a3
jnb27 <jnb27@njit.edu>
2021-03-23 17:44:53 -0400

    Mid Presentation

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import logging
 import os
 import random
 import pika
+import json
 from flask_rabmq import RabbitMQ
 #from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template, request
@@ -12,13 +13,15 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-#credentials = pika.PlainCredentials('admin', 'admin')
-#connection = pika.BlockingConnection(
-#   pika.ConnectionParameters(host ='25.121.196.54', port=5672, credentials=credentials))
-#channel = connection.channel()
+credentials = pika.PlainCredentials('admin', 'admin')
+connection = pika.BlockingConnection(
+   pika.ConnectionParameters(host ='25.121.196.54', port=5672, virtual_host = "vh1", credentials=credentials))
+channel = connection.channel()
 
-#channel.queue_declare(queue='hello')
-#channel.queue_declare(queue='bye')
+channel.queue_declare(queue='hello')
+channel.queue_declare(queue='bye')
+
+
 @app.route('/',methods = ['GET', 'POST'])
 def newUser():
     return render_template('register.html')
@@ -27,8 +30,9 @@ def form():
     try:
     	newPass = str(request.form['password'])
     	newInfo = str(request.form['username'])
-    	#channel.basic_publish(exchange='', routing_key='hello', body=newInfo)
-    	print(newPass,newInfo)
+    	message = {"from": "front", "reason": "create", "user": newInfo, "password": newPass}
+    	msg_json = json.dumps(message)
+    	channel.basic_publish(exchange='', routing_key='hello', body=msg_json)
     	return render_template('login.html')
     except KeyError:
     	return render_template('login.html')	
@@ -37,7 +41,7 @@ def start():
     try:
     	passw = str(request.form['password'])
     	info = str(request.form['username'])
-    	#channel.basic_publish(exchange='', routing_key='hello', body=nfo)
+    	channel.basic_publish(exchange='', routing_key='hello', body=nfo)
     	print(info,passw)
     	return render_template('index.html')
     except KeyError:

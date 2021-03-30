22dfec6cfeb01bf1f945fbf5136cf51ac230ba9b
jnb27 <jnb27@njit.edu>
2021-03-29 20:58:16 -0400

    Playtesting things

--- rec.py
+++ rec.py
@@ -7,22 +7,17 @@ def callback(ch, method, properties, body):
     if info.get("reason") == "create":
     #This is when we get something to turn into SQL Statement add AND process register
       query = "INSERT INTO test(user, pass) VALUES( " + info["user"] + ", " + info["password"] + ")"
-      message = {'create':query, 'reason':'query'}
+      message = {'create':query, 'reason':'query2'}
       msgjson= json.dumps(message)
       channel.basic_publish(exchange='', routing_key='hello', body=msgjson)
       
     if info.get("reason") == "login":
     #This is when we get something to turn into SQL Statement add AND process login
     	query = "SELECT * FROM test WHERE user=" + info['user'] + " AND pass=" + info['password']
-    	message = {'login':query, 'reason':'query'}
+    	message = {'login':query, 'reason':'query2'}
     	msgjson = json.dumps(message)
     	channel.basic_publish(exchange='', routing_key='hello', body=msgjson)
     
-
-
-#def callback2(cd, method, propoerties, body):
-	
-
 
 
 creds = pika.PlainCredentials('admin', 'admin')
